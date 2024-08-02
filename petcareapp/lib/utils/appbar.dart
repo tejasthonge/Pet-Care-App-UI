135756c8539bb52824c44070f44a3c58424598e3
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2024-08-03 02:02:18 +0530

    Update appbar.dart

--- petcareapp/lib/utils/appbar.dart
+++ petcareapp/lib/utils/appbar.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:petcareapp/constants/style/colors.dart';
+
 import 'package:petcareapp/constants/style/fonts.dart';
 
 AppBar buildAppBar({
